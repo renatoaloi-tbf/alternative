dbb95dc3e9cd9119fe0d8437b6362c34af809b8f
Renato Aloi <renato.aloi@tbfconsultoria.com.br>
2018-07-10 20:20:40 -0300

    Finalizando modal

--- src/components/Documentation/DocumentationModal.js
+++ src/components/Documentation/DocumentationModal.js
@@ -8,6 +8,13 @@ import {Text, Button, DatePicker, Modal} from '~/components/shared';
 import { TouchableHighlight, View, StyleSheet } from 'react-native';
 import { Icon } from '../../components/shared/Icon';
 
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+
+
+import Intl from 'intl';
+
+require( 'intl/locale-data/jsonp/pt' );
+
 const enhance = compose(
   setPropTypes({
     title: string,
@@ -32,38 +39,123 @@ export const DocumentationModal = enhance(
       const tableData = {
           tableHead: [ 'Análise', 'Leite Fornecido', 'Padrão' ],
           tableRows: [
-              [ 'Gordura', '3,94', 'check', 'Máx 4,0' ]
+            [ 'Gordura', '3.94', 'L', '4.0' ],
+              [ 'Proteína', '8.52', 'L', '5.0' ],
+              [ 'ESD', '8.92', 'L', '-3.0' ],
+              [ 'CBT', '392', 'L', '-2.9' ],
+              [ 'EST', '12.86', 'L', '-8.4' ],
+              [ 'CCS', '278', 'L', '-8.4' ]
           ]
       }
+      //alignSelf: 'flex-end'
+      const xizinho = <Icon style={{ color: '#6d6d6d' }} size={15} opacity={0.00} name="check" />;
     return (
       <Modal visible={visible} close={close}>
         <WrapperModal>
-          <Content>
-            <TopBar>
+
+            <TopBar style={{ borderWidth: 0, borderColor: 'blue', borderStyle: 'solid' }}>
                 <Icon size={100} opacity={0.00} color="#FFFFFF" name="alert" style={{ marginTop: 15, marginBottom: 15 }} />
             </TopBar>
-            <Body>
+
+            <Body style={{ borderWidth: 0, borderColor: 'green', borderStyle: 'solid' }}>
                 <Titulo size={20} weight={'bold'}>Resultados de análises do leite fora das especificações</Titulo>
                 <SubTitulo size={14} color={'#777777'}>Parâmetro da IN62 de Janeiro de 2018</SubTitulo>
+                <View>
+                  <Table borderStyle={{borderWidth: 0 }}>
+                    <Row data={tableData.tableHead} style={{ marginBottom: 5 }} textStyle={{ color: '#000000', fontWeight: 'bold' }} />
+                    {/* <Rows data={tableData.tableRows} style={{ marginBottom: 5 }} textStyle={{ color: '#777777' }} /> */}
+                    {
+                      tableData.tableRows.map((rowData, index) => (
+                        <TableWrapper key={index} style={{ flexDirection: 'row' }} >
+                          {
+                            rowData.map((cellData, cellIndex) => {
+                              let cell = '';
+                              let cellval = 0;
+                              switch(cellIndex)
+                              {
+                                case 1:
+                                  cellval = new Intl.NumberFormat('pt-BR', { style: 'decimal', minimumFractionDigits: 2 }).format(cellData);
+                                  cell = cellval;
+                                  break;
+                                case 2:
+                                  cell = xizinho;
+                                  break;
+                                case 3:
+                                  cellval = new Intl.NumberFormat('pt-BR', { style: 'decimal', minimumFractionDigits: 2 }).format(cellData > 0 ? cellData : (cellData * -1.0));
+                                  cell = (cellData > 0 ? 'Máx ' + cellval : 'Min ' + cellval);
+                                  break;
+                                default:
+                                  cell = cellData;
+                              }                              
+                              return (<Cell key={cellIndex} data={cell} textStyle={{ color: '#777777', marginTop: 5 }}/>);
+                            })
+                          }
+                        </TableWrapper>
+                      ))
+                    }
+                  </Table>
+                </View>
+                <Legenda size={14} color={'#777777'}>*(média do leite fornecido)</Legenda>
             </Body>
-          </Content>
+
+
+            <Footer style={{ borderWidth: 0, borderColor: 'purple', borderStyle: 'solid' }}>
+              <BotaoEsquerda>
+                <Text>Preencher checklist</Text>
+              </BotaoEsquerda>
+              <BotaoDireita>
+                <Text>Estou ciente</Text>
+              </BotaoDireita>
+            </Footer>
+
+         
+
+          
+          
+
         </WrapperModal>
       </Modal>
     );
   }
 );
 
+const BotaoEsquerda = styled.TouchableHighlight`
+  padding-top: 10;
+  width: 50%;
+  border-top-color: #e6e6e6;
+  border-top-width: 1;
+  height: 50;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const BotaoDireita = styled.TouchableHighlight`
+  padding-top: 10;
+  width: 50%;
+  border-left-color: #e6e6e6;
+  border-left-width: 1;
+  border-top-color: #e6e6e6;
+  border-top-width: 1;
+  height: 50;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Footer = styled.View`
+  margin-top: 10;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const WrapperModal = styled.View`
   background-color: ${props => props.theme.bg};
   width: 80%;
   height: 90%;
   border-radius: 10;
-`;
-
-const Content = styled.View`
-  padding: 0;
-  border-top-left-radius: 10;
-  border-top-right-radius: 10;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 0px solid red;
 `;
 
 const TopBar = styled.View`
@@ -77,7 +169,7 @@ const TopBar = styled.View`
 
 const Body = styled.View`
   background-color: #ffffff;
-  padding-top: 30;
+  padding-top: 20;
   padding-left: 20;
   padding-right: 20;
 `;
@@ -90,73 +182,7 @@ const SubTitulo = Text.extend`
   margin-bottom: 10;
 `;
 
-const styles = StyleSheet.create({
-    modal1: {
-        alignItems: 'center',
-        backgroundColor: '#ffbd00',
-        paddingTop: 20,
-        paddingBottom: 20,
-        borderTopWidth: 20,
-        borderTopColor: 'rgba(52, 52, 52, 0.8)',
-        borderRightWidth: 20,
-        borderRightColor: 'rgba(52, 52, 52, 0.8)',
-        borderLeftWidth: 20,
-        borderLeftColor: 'rgba(52, 52, 52, 0.8)'
-    },
-    text: {
-        color: '#3f2949',
-        marginTop: 10
-    },
-    modal2: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        padding: 30,
-        borderRightWidth: 20,
-        borderRightColor: 'rgba(52, 52, 52, 0.8)',
-        borderLeftWidth: 20,
-        borderLeftColor: 'rgba(52, 52, 52, 0.8)',
-        height: 50
-    },
-    title: {
-        fontSize: 15,
-        fontWeight: "900",
-        color: "#000000"
-    },
-    subTitle: {
-        marginTop: 10,
-        fontSize: 10
-    },
-    gridHead: { flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 10 },
-    gridHeadTextView: { width: 80, height: 40 },
-    gridHeadText: { color: "#000000" },
-
-    gridBody: { flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 10 },
-    gridBodyTextView: { width: 80, height: 15 },
-    styleIcon: { alignSelf: 'flex-end', color: '#6d6d6d' },
-    alertColor: { color: '#ffbd00' },
-
-    bottomView: {
-        width: '100%',
-        height: 190,
-        backgroundColor: '#FFFFFF',
-        position: 'absolute',
-        top: 500,
-        flexDirection: 'row',
-        borderRightWidth: 20,
-        borderRightColor: 'rgba(52, 52, 52, 0.8)',
-        borderLeftWidth: 20,
-        borderLeftColor: 'rgba(52, 52, 52, 0.8)',
-        borderBottomWidth: 130,
-        borderBottomColor: 'rgba(52, 52, 52, 0.8)'
-    },
-    buttonStyle: {
-        backgroundColor: '#ffffff',
-        width: 160,
-        borderTopWidth: 1,
-        borderColor: '#efefef',
-        borderRightWidth: 2,
-        alignItems: 'center',
-        paddingTop: 15,
-    }
-
-})
+const Legenda = Text.extend`
+  margin-top: 10;
+  font-style: italic;
+`;
