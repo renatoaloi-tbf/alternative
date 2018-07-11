c01f8f0a43bb27a4dd219b10f2a68fa9c16e0d7f
Renato Aloi <renato.aloi@tbfconsultoria.com.br>
2018-07-11 00:21:41 -0300

    Terminada tela do Modal

--- src/components/Documentation/DocumentationModal.js
+++ src/components/Documentation/DocumentationModal.js
@@ -5,11 +5,10 @@ import {compose, withProps, setPropTypes, defaultProps} from 'recompose';
 import {isArray} from 'lodash';
 import {Text, Button, DatePicker, Modal} from '~/components/shared';
 
-import { TouchableHighlight, View, StyleSheet } from 'react-native';
+import { TouchableHighlight, View, StyleSheet, Text as TextNative } from 'react-native';
 import { Icon } from '../../components/shared/Icon';
 
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
-
 
 import Intl from 'intl';
 
@@ -39,24 +38,22 @@ export const DocumentationModal = enhance(
       const tableData = {
           tableHead: [ 'Análise', 'Leite Fornecido', 'Padrão' ],
           tableRows: [
-            [ 'Gordura', '3.94', 'L', '4.0' ],
-              [ 'Proteína', '8.52', 'L', '5.0' ],
-              [ 'ESD', '8.92', 'L', '-3.0' ],
-              [ 'CBT', '392', 'L', '-2.9' ],
-              [ 'EST', '12.86', 'L', '-8.4' ],
-              [ 'CCS', '278', 'L', '-8.4' ]
+            [ 'Gordura', '3.94', '4.0' ],
+              [ 'Proteína', '8.52', '5.0' ],
+              [ 'ESD', '8.92', '-3.0' ],
+              [ 'CBT', '392', '-2.9' ],
+              [ 'EST', '12.86', '-8.4' ],
+              [ 'CCS', '278', '-8.4' ]
           ]
       }
-      //alignSelf: 'flex-end'
-      const xizinho = <Icon style={{ color: '#6d6d6d' }} size={15} opacity={0.00} name="check" />;
+      const checkNormal = <Icon style={{ color: '#6d6d6d', marginRight: 20 }} size={15} opacity={0.00} name="check" />;
+      const xisAlert = <Icon style={{ color: '#ffbd00', marginRight: 20 }} size={15} opacity={0.00} name="window-close" />;
     return (
       <Modal visible={visible} close={close}>
         <WrapperModal>
-
             <TopBar style={{ borderWidth: 0, borderColor: 'blue', borderStyle: 'solid' }}>
                 <Icon size={100} opacity={0.00} color="#FFFFFF" name="alert" style={{ marginTop: 15, marginBottom: 15 }} />
             </TopBar>
-
             <Body style={{ borderWidth: 0, borderColor: 'green', borderStyle: 'solid' }}>
                 <Titulo size={20} weight={'bold'}>Resultados de análises do leite fora das especificações</Titulo>
                 <SubTitulo size={14} color={'#777777'}>Parâmetro da IN62 de Janeiro de 2018</SubTitulo>
@@ -65,54 +62,76 @@ export const DocumentationModal = enhance(
                     <Row data={tableData.tableHead} style={{ marginBottom: 5 }} textStyle={{ color: '#000000', fontWeight: 'bold' }} />
                     {/* <Rows data={tableData.tableRows} style={{ marginBottom: 5 }} textStyle={{ color: '#777777' }} /> */}
                     {
-                      tableData.tableRows.map((rowData, index) => (
-                        <TableWrapper key={index} style={{ flexDirection: 'row' }} >
-                          {
-                            rowData.map((cellData, cellIndex) => {
-                              let cell = '';
-                              let cellval = 0;
-                              switch(cellIndex)
+                      tableData.tableRows.map((rowData, index) => {
+
+                          let rowLeite = 0;
+                          let isOffSpecs = false;
+                          rowData.map((cellData, cellIndex) => {
+                            if (cellIndex === 1)
+                            {
+                              rowLeite = cellData;
+                            } 
+                            if (cellIndex === 2)
+                            {
+                              if (cellData > 0)
                               {
-                                case 1:
-                                  cellval = new Intl.NumberFormat('pt-BR', { style: 'decimal', minimumFractionDigits: 2 }).format(cellData);
-                                  cell = cellval;
-                                  break;
-                                case 2:
-                                  cell = xizinho;
-                                  break;
-                                case 3:
-                                  cellval = new Intl.NumberFormat('pt-BR', { style: 'decimal', minimumFractionDigits: 2 }).format(cellData > 0 ? cellData : (cellData * -1.0));
-                                  cell = (cellData > 0 ? 'Máx ' + cellval : 'Min ' + cellval);
-                                  break;
-                                default:
-                                  cell = cellData;
-                              }                              
-                              return (<Cell key={cellIndex} data={cell} textStyle={{ color: '#777777', marginTop: 5 }}/>);
-                            })
-                          }
-                        </TableWrapper>
-                      ))
+                                isOffSpecs = (rowLeite > cellData);
+                              } 
+                              else
+                              {
+                                isOffSpecs = (rowLeite < (cellData * -1.0));
+                              }
+                            } 
+                          });
+
+                          return (
+                            <TableWrapper key={index} style={{ flexDirection: 'row' }} >
+                              {
+                                rowData.map((cellData, cellIndex) => {
+                                  let cell = '';
+                                  let cellval = 0;
+                                  switch(cellIndex)
+                                  {
+                                    case 1:
+                                      rowLeite = cellData;
+                                      cellval = new Intl.NumberFormat('pt-BR', { style: 'decimal', minimumFractionDigits: 2 }).format(cellData);
+                                      cell = <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}><Text color={isOffSpecs ? '#ffbd00' : '#6d6d6d'}>{cellval}</Text>{isOffSpecs ? xisAlert : checkNormal}</View>;
+                                      break;
+                                    case 2:
+                                      if (cellData > 0)
+                                      {
+                                        isOffSpecs = (rowLeite > cellData);
+                                      }
+                                      else
+                                      {
+                                        isOffSpecs = (rowLeite < (cellData * -1.0));
+                                      }
+                                      cellval = new Intl.NumberFormat('pt-BR', { style: 'decimal', minimumFractionDigits: 2 }).format(cellData > 0 ? cellData : (cellData * -1.0));
+                                      cell = (cellData > 0 ? 'Máx ' + cellval : 'Min ' + cellval);
+                                      break;
+                                    default:
+                                      cell = cellData;
+                                  }                              
+                                  return (<Cell key={cellIndex} data={cell} textStyle={{ color: `${isOffSpecs ? '#ffbd00' : '#6d6d6d'}`, marginTop: 5 }}/>);
+                                })
+                              }
+                            </TableWrapper>
+                          );
+                        }
+                      )
                     }
                   </Table>
                 </View>
                 <Legenda size={14} color={'#777777'}>*(média do leite fornecido)</Legenda>
             </Body>
-
-
             <Footer style={{ borderWidth: 0, borderColor: 'purple', borderStyle: 'solid' }}>
-              <BotaoEsquerda>
+              <BotaoEsquerda onPress={onPress}>
                 <Text>Preencher checklist</Text>
               </BotaoEsquerda>
-              <BotaoDireita>
+              <BotaoDireita onPress={onPress}>
                 <Text>Estou ciente</Text>
               </BotaoDireita>
             </Footer>
-
-         
-
-          
-          
-
         </WrapperModal>
       </Modal>
     );
